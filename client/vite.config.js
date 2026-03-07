e1bbb2740f9cfb7878dff22fd81cff0c3ab4b7a4
vamshi-987 <vamshivardhan987@gmail.com>
2026-03-07 16:15:38 +0530

    vite config imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,7 @@
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
